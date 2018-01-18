2d81138280e51dc09d9c1aca26bc002329f168f7
aliciapflaumer <alpfla7@gmail.com>
2018-01-18 09:04:06 -0500

    Re-added about route to app
    - Added {{welcome}} template

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  welcome : string;
+  constructor() {
+    this.welcome = "Welcome to the about page";
+   }
 
   ngOnInit() {
   }
